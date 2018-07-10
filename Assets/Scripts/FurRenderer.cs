6b10a9e8736f0dcffb4bed64b4ad2eb0e81f30d2
LitLov3 <912778422@qq.com>
2018-07-11 00:15:41 +0800

    Init

---
+++ Assets/Scripts/FurRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FurRenderer : MonoBehaviour {
+
+	public Shader furShader;
+	private Material mat;
+    private CommandBuffer cb;
+    private Camera cam;
+
+    public Mesh testmesh;
+    public Material std;
+
+	private void Start()
+	{
+		mat = new Material(furShader);
+        cam = Camera.main;
+        cb = new CommandBuffer();
+        cb.name = "Z Fur";
+        cam.AddCommandBuffer(CameraEvent.AfterGBuffer, cb);
+
+        cb.DrawMesh(testmesh, Matrix4x4.identity, std);
+	}
+
+    private void OnPreRender()
+    {
+        
+    }
+}
